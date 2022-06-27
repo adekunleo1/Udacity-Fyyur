b60661b903cef04e8da6de67e8b3c9e90b4e74e2
adekunle ogunleye <adekunle.ogunleye1@gmail.com>
2022-06-27 22:30:25 +0100

    Update

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from dbm.dumb import _Database
 import os
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -13,4 +14,4 @@ DEBUG = True
 # TODO IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:abcdef@127.0.0.1:5432/udacityfyyurdb'
 
-SQLALCHEMY_TRACK_MODIFICATIONS = "False"
+SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
--- models.py
+++ models.py
@@ -1,8 +1,7 @@
-from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_migrate import Migrate
-
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 moment = Moment(app)
@@ -18,7 +17,6 @@ migrate = Migrate(app, db)
 class Show(db.Model):
     # Table name
     __tablename__ = 'shows'
-
     id = db.Column(db.Integer, primary_key=True)
     artist_id = db.Column(db.Integer, db.ForeignKey(
         'artists.id', ondelete='CASCADE'), nullable=False)
@@ -28,18 +26,19 @@ class Show(db.Model):
     venue = db.relationship('Venue', back_populates='artists_show',
                             lazy=True, cascade='all, delete', passive_deletes=True)
     artist = db.relationship('Artist', back_populates='venues_show',
-                            lazy=True, cascade='all, delete', passive_deletes=True)
+                             lazy=True, cascade='all, delete', passive_deletes=True)
+
 
 class Venue(db.Model):
     __tablename__ = 'venues'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
-    city = db.Column(db.String(100), nullable=False)
-    state = db.Column(db.String(3), nullable=False)
-    address = db.Column(db.String(250), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(2), nullable=False)
+    address = db.Column(db.String(300), nullable=False)
     genres = db.Column(db.ARRAY(db.String()), nullable=False)
-    phone = db.Column(db.String(18), nullable=True)
+    phone = db.Column(db.String(15), nullable=True)
     website = db.Column(db.String(500), nullable=True)
     image_link = db.Column(db.String(500), nullable=True)
     facebook_link = db.Column(db.String(), nullable=True)
@@ -47,12 +46,13 @@ class Venue(db.Model):
     seeking_description = db.Column(db.String(500), nullable=True)
 
     artists_show = db.relationship(
-        "Show", back_populates="venues", cascade='all, delete')
+        "Show", back_populates="venue", cascade='all, delete')
 
     def __repr__(self):
         return f"\n<Venue id: {self.id} name: {self.name}>"
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
 
 class Artist(db.Model):
     __tablename__ = 'artists'
@@ -60,8 +60,8 @@ class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
     city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(3), nullable=False)
-    phone = db.Column(db.String(18), nullable=True, unique=True)
+    state = db.Column(db.String(2), nullable=False)
+    phone = db.Column(db.String(15), nullable=True, unique=True)
     genres = db.Column(db.ARRAY(db.String()), nullable=False)
     image_link = db.Column(db.String(500), nullable=True)
     facebook_link = db.Column(db.String(500), nullable=True)
@@ -70,7 +70,7 @@ class Artist(db.Model):
     seeking_description = db.Column(db.String(300), nullable=True)
 
     venues_show = db.relationship(
-        "Show", back_populates="artists", cascade='all, delete')
+        "Show", back_populates="artist", cascade='all, delete')
 
     def __repr__(self):
         return f"\n<Artist id: {self.id} name: {self.name}>"
